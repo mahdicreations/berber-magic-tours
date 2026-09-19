0919f44939646f2077b388d1b6c12ae45efaa7cd
mahdi <eelmahdi2@alumno.uned.es>
2026-09-19 15:21:22 +0100

    vm2

--- tours/send-mail.php
+++ tours/send-mail.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 // SMTP Configuration
 $smtp_user = 'info@berber-magic-tours.com';
 $smtp_pass = '@berber-magic-tours2026';
-$to_email  = 'javarx@gmail.com';
+$to_email  = 'berbermagictours@gmail.com';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     echo json_encode(['status' => 'error', 'message' => 'Invalid request method.']);
